0cbcb9023ea0abf7afcfcdd184fbf97b86813f9e
Bruce Collie <brucecollie82@gmail.com>
2018-03-21 14:32:13 +0000

    VLOSS

--- resnet-data/plot_vloss.py
+++ resnet-data/plot_vloss.py
@@ -4,6 +4,7 @@ import csv
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import numpy as np
 
 WALL = 'Wall time'
 STEP = 'Step'
@@ -11,29 +12,34 @@ VALUE = 'Value'
 
 plt.style.use('ggplot')
 
+def key(rule):
+    if rule == 'SGDMomentum': return 'momentum'
+    elif rule == 'SGDNesterovMomentum': return 'nesterov'
+    else: return rule.lower()
+
 def file_name(rule, rate=None):
     if rate is not None:
-        return "{}_loss_{}.csv".format(rule, rate)
+        return "{}_loss_{}.csv".format(key(rule), rate)
     else:
-        return "{}_decay_vloss.csv".format(rule)
+        return "{}_decay_loss.csv".format(key(rule))
 
 def rate_legend(name):
-    rule = name.split('_')[0]
+    rule = key(name)
     val = name.split('_')[-1].split('.')[0]
     return rule + ' ' + val[:1] + '.' + val[1:]
 
 def best_rates():
     return {
-        # "adadelta" : "001",
-        "adagrad" : "001",
-        "adam" : "00001",
-        # "adamax" : "0001",
-        "eve" : "00001",
-        # "momentum" : "001",
-        # "nesterov" : "001",
-        "rmsprop" : "00001",
-        "sgd" : "001",
-        "amsgrad" : "00001"
+        "Adadelta" : "001",
+        "Adagrad" : "001",
+        "Adam" : "00001",
+        "Adamax" : "0001",
+        "AMSgrad" : "00001",
+        "Eve" : "00001",
+        "RMSprop" : "00001",
+        "SGD" : "001",
+        # "SGDMomentum" : "001",
+        "SGDNesterovMomentum" : "001",
     }
 
 def plot_vloss():
@@ -44,12 +50,13 @@ def plot_vloss():
     rates = best_rates()
     for rule in rates:
         frame = pd.read_csv(file_name(rule))
-        ax.plot(frame[STEP], frame[VALUE])
+        ax.plot(frame[STEP][::5], frame[VALUE][::5])
         legends.append(rule)
     ax.legend(legends)
     ax.set_ylabel('Validation Loss')
     ax.set_xlabel('Training Step')
-    plt.savefig('resnet_loss.pdf')
+    plt.show()
+    # plt.savefig('resnet_loss.pdf')
     
 if __name__ == "__main__":
     plot_vloss()
